919270017cec4ba9d1b13acf85a50e0cb5ac9b5e
Tristan Theron <tristantheron@gmail.com>
2025-02-19 11:57:15 +0200

    Fixed server running after test

--- peertest.sh
+++ peertest.sh
@@ -1,1 +1,2 @@
-go run ./cmd/gotorrentserver/main.go -- test peer ./test_folder-d984f67af9917b214cd8b6048ab5624c7df6a07a.torrent
+rm -rf TestBundle
+go run ./cmd/gotorrentserver/main.go -- test session ./test_folder-d984f67af9917b214cd8b6048ab5624c7df6a07a.torrent
